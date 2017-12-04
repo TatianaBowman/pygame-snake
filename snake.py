3c3c7ce4b55108df94afd6a4cf6f5b2f9b1356cf
TatianaBowman <34111011+TatianaBowman@users.noreply.github.com>
2017-12-04 14:39:42 -0500

    Update snake.py
    color change

--- snake.py
+++ snake.py
@@ -30,11 +30,11 @@ DIRECTION_DOWN = (0, 1)
 # Background color of the snake grid.
 COLOR_BACKGROUND = (255, 255, 255)  # rgb color for white
 # This is the color of the snake's head. 
-COLOR_SNAKE_HEAD = (255, 0, 0)      # rgb color for red
+COLOR_SNAKE_HEAD = (203, 28, 137)      # rgb color for red
 # This is the color of the rest of the snake.
-COLOR_SNAKE = (0, 255, 0)           # rgb color for green
+COLOR_SNAKE = (25, 94, 174)           # rgb color for green
 # This is the color for the snake's food.
-COLOR_FOOD = (255, 200, 0)          # rgb color for orange
+COLOR_FOOD = (244, 118, 87)          # rgb color for orange
 # This is the color for the game over text.
 COLOR_GAME_OVER_TEXT = (0, 0, 0)    # rgb color for black
 
@@ -193,7 +193,9 @@ def process_events(direction, game_over):
     for event in pygame.event.get():
         # Quit the program when the user presses the x in the corner of the window.
         if event.type == pygame.QUIT:
-            sys.exit()
+             pygame.display.quit()
+             pygame.quit()
+             sys.exit()
         # Process events when the user presses a key on the keyboard.
         # https://www.pygame.org/docs/ref/key.html
         elif event.type == pygame.KEYDOWN:
@@ -257,3 +259,4 @@ def start_game():
 
 # Start the snake game.
 start_game()
+
